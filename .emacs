3d93647deb8acd900f9e850d82998dfbf6d6851e
Bosirex <dennis.dancan@gmail.com>
2023-05-24 03:05:37 -0700

    simple_shell

--- .emacs
+++ .emacs
@@ -2,9 +2,7 @@
       c-basic-offset 8
       tab-width 8
       indent-tabs-mode t)
-
 (require 'whitespace)
 (setq whitespace-style '(face empty lines-tail trailing))
 (global-whitespace-mode t)
-
 (setq column-number-mode t)
---
+++ shell_prompt.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+#include <sys/wait.h>
+
+
+/**
+ * shell_prompt - fn for executing terminal command
+ * @av: argument vector (string array)
+ * @env: argument vector (string array)
+*/
+
+void shell_prompt(char **av, char **env)
+{
+    char *user_input = NULL;
+    int k, child_status;
+    size_t m = 0;
+    ssize_t input_char;
+    char *argv[] = {NULL, NULL};
+    pid_t child_pid;
+
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("simple_shell$ ");
+        input_char = getline(&user_input, &m, stdin);
+        if (input_char == -1)
+        {
+            free(user_input);
+            exit(EXIT_FAILURE);
+        }
+        k = 0;
+        while (user_input[k])
+        {
+            if (user_input[k] == '\n')
+                user_input[k] = 0;
+            k++;
+        }
+        argv[0] = user_input;
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            free(user_input);
+            exit(EXIT_FAILURE);
+        }
+        if (child_pid == 0)
+        {
+            if (execve(argv[0], argv, env) == -1)
+                printf("%s: No such file or directory\n", av[0]);
+            
+        }
+        else
+            wait(&child_status);
+    }
+}
+/**
+ * main - function
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment
+ * 
+ * Return: 0
+*/
+int main(int ac, char **av, char **env)
+{
+    if (ac == 1)
+    shell_prompt(av, env);
+    return (0);
+}
