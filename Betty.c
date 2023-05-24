abe63b50b180b12dfa50eae6c90117ffd109e38e
Bosirex <dennis.dancan@gmail.com>
2023-05-24 01:34:36 -0700

    simple_shell

---
+++ Betty.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * sum - adds two numbers
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * Return: sum of the two integers
+*/
+int sum(int a, int b)
+{
+    int sum = a + b;
+    return (sum);
+}
